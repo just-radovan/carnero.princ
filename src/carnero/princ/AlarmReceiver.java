80cb4bef03d1cc64822991aee12c5dc6253ce444
carnero <carnero@carnero.cc>
2013-09-28 11:58:51 +0200

    change: conserve battery

--- src/carnero/princ/AlarmReceiver.java
+++ src/carnero/princ/AlarmReceiver.java
@@ -6,10 +6,17 @@ import android.content.Intent;
 import android.util.Log;
 import carnero.princ.common.Constants;
 
+import java.util.Calendar;
+
 public class AlarmReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		Calendar calendar = Calendar.getInstance();
+		if (calendar.get(Calendar.HOUR_OF_DAY) < 10) { // do not download when pub is closed
+			return;
+		}
+
 		Intent serviceIntent = new Intent(context, DownloadService.class);
 		context.startService(serviceIntent);
 	}
